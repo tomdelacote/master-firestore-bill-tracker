85992fefdeec8b17ec6eab2ed7023cb179eded55
Jorge Vergara <javebratt@gmail.com>
2019-01-27 15:50:25 -0500

    NUKE everything for Ioni 4 final update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { firebaseConfig } from './credentials';
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireAuthModule,
-    AngularFirestoreModule,
-    AngularFireStorageModule,
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,62 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { BillService } from '../services/bill.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
-  public billList: Observable<any>;
-  constructor(
-    private actionSheetCtrl: ActionSheetController,
-    private billService: BillService,
-    private router: Router
-  ) {}
+export class HomePage {
 
-  ngOnInit() {
-    this.billList = this.billService.getBillList().valueChanges();
-  }
-
-  async moreBillOptions(billId): Promise<void> {
-    const action = await this.actionSheetCtrl.create({
-      header: 'Modify your bill',
-      buttons: [
-        {
-          text: 'Delete',
-          role: 'destructive',
-          icon: 'trash',
-          handler: () => {
-            this.billService.removeBill(billId);
-          },
-        },
-        {
-          text: 'More details',
-          icon: 'play',
-          handler: () => {
-            this.router.navigate(['/bill-detail', billId]);
-          },
-        },
-        {
-          text: 'Mark as Paid!',
-          icon: 'checkmark',
-          handler: () => {
-            this.billService.payBill(billId);
-          },
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          icon: 'close',
-          handler: () => {
-            console.log('Cancel clicked');
-          },
-        },
-      ],
-    });
-    action.present();
-  }
 }
